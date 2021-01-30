d109e77609c3c6ac4597502cc0ee24f7d4185c43
bluecover <gurity@163.com>
2021-01-30 23:10:07 +0800

    Add samples for business data

---
+++ eastmoney/stock/samples/business.sh
@@ -0,0 +1,7 @@
+curl 'http://f10.eastmoney.com/BusinessAnalysis/BusinessAnalysisAjax?code=SH688299' \
+  -H 'Accept: */*' \
+  -H 'Referer: http://f10.eastmoney.com/f10_v2/BusinessAnalysis.aspx?code=sh688299' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36' \
+  --compressed \
+  --insecure
