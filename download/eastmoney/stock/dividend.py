54d5aa63f68685dc222349098a9c0ab7b389424a
bluecover <gurity@163.com>
2021-02-05 15:18:20 +0800

    Get and store stock dividend history

---
+++ download/eastmoney/stock/dividend.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+    获取股息率
+
+    返回数据样例: samples/dividend.json
+"""
+
+import pymongo
+import requests
+
+from download.eastmoney.stock import db
+
+
+def _to_float(s):
+    if not s or s == "-" or s == "--":
+        return 0.0
+    return float(s)
+
+
+def get_dividend_history(session, stock_code):
+    url = "http://dcfm.eastmoney.com/EM_MutiSvcExpandInterface/api/js/get"
+    headers = {
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) Chrome/88.0.4324.146 Safari/537.36",
+        "Accept": "*/*",
+        "Referer": "http://data.eastmoney.com/",
+        "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    }
+    params = {
+        "st": "ReportingPeriod",
+        "sr": "-1",
+        "ps": "50",
+        "p": "1",
+        "type": "DCSOBS",
+        "cmd": stock_code,
+        "token": "894050c76af8597a853f5b408b759f5d",
+    }
+    resp = session.get(url, headers=headers, params=params)
+    data = resp.json()
+    return data
+
+
+def _extract(dividend_data):
+    return [
+        {
+            "date": d["ReportingPeriod"][0:10],
+            "percent": round(_to_float(d.get("GXL", 0.0)) * 100, 2),
+        }
+        for d in dividend_data
+    ]
+
+
+def get_and_store_dividend():
+    sess = requests.Session()
+    write_op_list = []
+    stock_cols = db.Stock.find(projection=["code"])
+    for stock in stock_cols:
+        dividend_history = _extract(get_dividend_history(sess, stock["code"]))
+        write_op_list.append(
+            pymongo.UpdateOne(
+                {"_id": stock["_id"]},
+                {"$set": {"dividend_history": dividend_history}},
+            )
+        )
+
+    db.Stock.bulk_write(write_op_list)
+
+
+if __name__ == "__main__":
+    get_and_store_dividend()
---
+++ download/eastmoney/stock/samples/req/dividend.sh
@@ -0,0 +1,8 @@
+curl 'http://dcfm.eastmoney.com/EM_MutiSvcExpandInterface/api/js/get?st=ReportingPeriod&sr=-1&ps=50&p=1&type=DCSOBS&cmd=601003&token=894050c76af8597a853f5b408b759f5d' \
+  -H 'Connection: keep-alive' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36' \
+  -H 'Accept: */*' \
+  -H 'Referer: http://data.eastmoney.com/' \
+  -H 'Accept-Language: en,zh-CN;q=0.9,zh;q=0.8' \
+  --compressed \
+  --insecure
