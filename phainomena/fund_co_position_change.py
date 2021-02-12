7fe1c36c43d78d49f26f02b92271801ba16c352f
bluecover <gurity@163.com>
2021-02-13 00:35:31 +0800

    Reimplement fund company position change

--- phainomena/fund_co_position_change.py
+++ phainomena/fund_co_position_change.py
@@ -12,64 +12,63 @@ from phainomena import db
 
 REPORT_DATE = "2020-12-31"
 
-fund_position_change = {
+FUND_POSITION_CHANGE = {
     "enter": [
         {
             "name": "金螳螂",
             "code": "002081",
-            "volume": 10000,
-            "value": 1000025,
-            "volume_in_float": 0.25,
-            "total_percent": 6.6,
-            "fund_count": 5,
+            "funds": ["123012", "321002", "001002"],
         }
     ],
     "exit": [],
 }
 
 
-def diff_co_stock_position_change(new_position_dict, old_position_dict):
+def diff_company_position(new_position_dict, old_position_dict):
     enter_stock_codes = set(new_position_dict) - set(old_position_dict)
-    # exit_stock_codes = set(old_position_dict) - set(new_position_dict)
+    exit_stock_codes = set(old_position_dict) - set(new_position_dict)
 
-    enter_list, exit_list = [], []
+    enter_list = [
+        {
+            "code": code,
+            "name": new_position_dict[code]["name"],
+            "total_percent": new_position_dict[code]["total_percent"],
+            "volume_in_float": new_position_dict[code]["volume_in_float"],
+            "funds": new_position_dict[code]["funds"],
+        }
+        for code in enter_stock_codes
+    ]
+    enter_list.sort(key=lambda x: x["total_percent"], reverse=True)
 
-    for code in enter_stock_codes:
-        volume, value, volume_in_float, total_percent = 0, 0, 0, 0
-        fund_position = new_position_dict[code]
-        for fp in fund_position:
-            sp = fp["position"]
-            volume += sp["volume"]
-            value += sp["value"]
-            volume_in_float += sp.get("volume_in_float", 0.0)
-            total_percent += sp["percent"]
-        enter_list.append(
-            {
-                "code": code,
-                "name": fund_position[0]["position"]["name"],
-                "volume": round(volume, 2),
-                "value": round(value, 2),
-                "volume_in_float": round(volume_in_float, 2),
-                "total_percent": round(total_percent, 2),
-                "fund_count": len(fund_position),
-            }
-        )
-
-    enter_list.sort(key=lambda x: x["volume_in_float"], reverse=True)
+    exit_list = [
+        {
+            "code": code,
+            "name": old_position_dict[code]["name"],
+            "total_percent": old_position_dict[code]["total_percent"],
+            "volume_in_float": old_position_dict[code]["volume_in_float"],
+            "funds": old_position_dict[code]["funds"],
+        }
+        for code in exit_stock_codes
+    ]
+    exit_list.sort(key=lambda x: x["total_percent"], reverse=True)
 
     return enter_list, exit_list
 
 
+def _position_to_dict(position):
+    return {p["code"]: p for p in position}
+
+
 def obtain_company_position_change(report_date):
     co_position_change_list = []
-    co_position_col = db.FundCompanyPosition.find().sort("size", pymongo.DESCENDING)
-    for cop in co_position_col:
-        list_by_date = cop["list_by_date"]
-        if len(list_by_date) < 2 or list_by_date[0]["date"] != report_date:
+    co_positions = list(db.FundCompanyPosition.find().sort("size", pymongo.DESCENDING))
+    for cop in co_positions:
+        position_by_date = cop["position_by_date"]
+        if len(position_by_date) < 2 or position_by_date[0]["date"] != report_date:
             continue
-        enter_list, exit_list = diff_co_stock_position_change(
-            list_by_date[0]["stock_position"],
-            list_by_date[1]["stock_position"],
+        enter_list, exit_list = diff_company_position(
+            _position_to_dict(position_by_date[0]["position"]),
+            _position_to_dict(position_by_date[1]["position"]),
         )
         co_position_change_list.append(
             {
@@ -77,7 +76,7 @@ def obtain_company_position_change(report_date):
                 "co_name": cop["co_name"],
                 "co_size": cop["co_size"],
                 "enter": enter_list,
-                # "exit": exit_list,
+                "exit": exit_list,
             }
         )
 
