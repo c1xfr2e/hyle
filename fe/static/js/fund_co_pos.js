22a38f2d2274a608430e8a7df2bd336bacdc0507
bluecover <gurity@163.com>
2021-02-13 18:57:52 +0800

    Sort column

--- fe/static/js/fund_co_pos.js
+++ fe/static/js/fund_co_pos.js
@@ -1,4 +1,11 @@
 
+window.onload = () => {
+  openFundCompany(null, "80041198")
+  document.getElementById("80041198").className += " w3-red";
+}
+
+let positionData;
+
 function openFundCompany(evt, coid) {
   fetch(`/api/fundco/${coid}/position`)
     .then(response => response.json())
@@ -15,56 +22,78 @@ function openFundCompany(evt, coid) {
   }
 }
 
-let sortDirection = true;
-
-let positionData;
-
-window.onload = () => {
-  openFundCompany(null, "80041198")
-  document.getElementById("80041198").className += " w3-red";
-}
-
-function loadTableData(positionData) {
+function loadTableData(data) {
   const tableBody = document.getElementById("table_data")
   let dataHTML = "";
 
-  for (let p of positionData) {
-    let position_change = "";
+  for (let p of data) {
+    let positionChange = "";
     if (p.enter_count > 0 || p.exit_count > 0) {
-      position_change = `新进: <span style="color:#d50000">${p.enter_count}</span> 退出: <span style="color:green">${p.exit_count}</span>`
+      positionChange = `新进: <span style="color:#d50000">${p.enter_count}</span> 退出: <span style="color:green">${p.exit_count}</span>`
     }
     dataHTML += `<tr class="w3-hover-light-blue">
       <td><span style="display: inline-block; width: 80px;">${p.name}</span><span style="display: inline-block; color: gray;">${p.code}</span></td>
       <td>${p.volume_in_float}%</td>
       <td>${p.total_percent}%</td>
       <td>${p.funds.length}</td>
-      <td>${position_change}</td>
+      <td>${positionChange}</td>
     </tr>`;
   }
 
   tableBody.innerHTML = dataHTML
 }
 
-function sortColumn(columnName) {
-  const dataType = typeof positionData[0][columnName];
-  sortDirection = !sortDirection
+let sortDirection = true;
 
-  switch (dataType) {
-    case "number":
-      sortNumberColumn(sortDirection, columnName);
-      break;
-  }
-
-  loadTableData(positionData)
-
-  th = document.getElementById(columnName)
-
+let sortOptions = {
+  volume_in_float: {
+    direction: true,
+    sortFunc: (dir) => sortNumberColumn("volume_in_float", dir),
+  },
+  total_percent: {
+    direction: true,
+    sortFunc: (dir) => sortNumberColumn("total_percent", dir),
+  },
+  fund_count: {
+    direction: true,
+    sortFunc: sortFundCount
+  },
+  position_change: {
+    direction: false,
+    sortFunc: sortPositionChange
+  },
 }
 
-function sortNumberColumn(dir, col) {
+function sortColumn(columnName) {
+  op = sortOptions[columnName]
+  op.direction = !op.direction
+  op.sortFunc(op.direction)
+
+  loadTableData(positionData)
+}
+
+function sortNumberColumn(col, dir) {
   positionData = positionData.sort(
     (p1, p2) => {
       return dir ? p1[col] - p2[col] : p2[col] - p1[col]
+    }
+  );
+}
+
+function sortFundCount(dir) {
+  positionData = positionData.sort(
+    (p1, p2) => {
+      return dir? p1.funds.length - p2.funds.length : p2.funds.length - p1.funds.length
+    }
+  );
+}
+
+function sortPositionChange(dir) {
+  positionData = positionData.sort(
+    (p1, p2) => {
+      v1 = p1.enter_count * 1000 - p1.exit_count
+      v2 = p2.enter_count * 1000 - p2.exit_count
+      return dir? v1 - v2 : v2 - v1;
     }
   );
 }
