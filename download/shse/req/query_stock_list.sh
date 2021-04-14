2c4c6faf54eab5ad61702a51735402fd4f5f0d7a
bluecover <gurity@163.com>
2021-04-14 12:32:25 +0800

    Refactor download/shse

---
+++ download/shse/req/query_stock_list.sh
@@ -0,0 +1,11 @@
+curl 'http://query.sse.com.cn/security/stock/getStockListData.do?&isPagination=false&stockCode=&csrcCode=&areaName=&stockType=1&pageHelp.cacheSize=1&pageHelp.beginPage=1&pageHelp.pageSize=25&pageHelp.pageNo=1&_=1618366706729' \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36' \
+  -H 'Accept: */*' \
+  -H 'Referer: http://www.sse.com.cn/' \
+  -H 'Accept-Language: en,zh-CN;q=0.9,zh;q=0.8' \
+  -H 'Cookie: JSESSIONID=DE72E21086DAD8B89F458A70394797B3; yfx_c_g_u_id_10000042=_ck21041400010615701565791235575; VISITED_MENU=%5B%228528%22%5D; yfx_f_l_v_t_10000042=f_t_1618329666550__r_t_1618329666550__v_t_1618366705763__r_c_0' \
+  --compressed \
+  --insecure
--- download/shse/stock_list.py
+++ download/shse/stock_list.py
@@ -3,25 +3,6 @@
 
 """
     Query stock list from sse.com.cn
-
-    返回数据格式：
-    {
-        "result": [
-            {
-                "COMPANY_CODE": "600000",
-                "SECURITY_ABBR_A": "浦发银行",
-                "SECURITY_ABBR_B": "-",
-                "SECURITY_CODE_A": "600000",
-                "SECURITY_CODE_B": "-",
-                "totalShares": "2935208.04",
-                "totalFlowShares": "2810376.39",
-                "NUM": "1",
-                "TYPE": "主板A股",
-                "LISTING_DATE": "1999-11-10"
-                "endDate": "2019-08-02",
-            },
-        ]
-    }
 """
 
 import logging
@@ -30,6 +11,8 @@ import requests
 from collections import namedtuple
 from datetime import datetime
 from typing import List
+
+import db
 
 
 stock = namedtuple(
@@ -42,9 +25,39 @@ stock = namedtuple(
 )
 
 
-def _get_stock_list_by_page(page: int, stock_type) -> List[stock]:
+def get_stock_list_by_page(page: int, stock_type) -> List[stock]:
+    """
+    分页获取上交所股票列表
+
+    Args:
+        page: 页号 (首页为 1)
+        stock_type: 所属板块 (1:上海主板 2:B股 8:科创板)
+
+    Returns:
+        股票基本信息列表
+
+    上交所接口返回的数据格式：
+        {
+            "result": [
+                {
+                    "COMPANY_CODE": "600000",
+                    "SECURITY_ABBR_A": "浦发银行",
+                    "SECURITY_ABBR_B": "-",
+                    "SECURITY_CODE_A": "600000",
+                    "SECURITY_CODE_B": "-",
+                    "totalShares": "2935208.04",
+                    "totalFlowShares": "2810376.39",
+                    "NUM": "1",
+                    "TYPE": "主板A股",
+                    "LISTING_DATE": "1999-11-10"
+                    "endDate": "2019-08-02",
+                },
+            ]
+        }
+    """
+
     url = "http://query.sse.com.cn/security/stock/getStockListData.do"
-    # 必须有 Referer， 否则调用非法
+    # header 中必须有 Referer
     headers = {
         "Referer": "http://www.sse.com.cn/assortment/stock/list/share/",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) Chrome/75.0.3770.142 Safari/537.36",
@@ -54,10 +67,10 @@ def _get_stock_list_by_page(page: int, stock_type) -> List[stock]:
         "stockCode": "",
         "csrcCode": "",
         "areaName": "",
-        "stockType": stock_type,  # 1: 主板A股  2: B股  8: 科创板
+        "stockType": stock_type,  # 1:主板A股 2:B股 8:科创板
         "pageHelp.cacheSize": 1,
-        "pageHelp.beginPage": page,  # 页数， 每次请求递增
-        "pageHelp.pageSize": 50,  # 每次请求返回的股票数量， 最大 50
+        "pageHelp.beginPage": page,  # 页数，每次请求递增
+        "pageHelp.pageSize": 50,  # 每次请求返回的股票数量，最大 50
         "pageHelp.pageNo": 1,
     }
     resp = requests.get(url, headers=headers, params=params)
@@ -65,7 +78,12 @@ def _get_stock_list_by_page(page: int, stock_type) -> List[stock]:
         logging.error("request failed: url=%s, response=%s", url, resp)
         resp.raise_for_status()
         return None
-    result = resp.json()["result"]
+
+    try:
+        result = resp.json()["result"]
+    except Exception as e:
+        logging.error(resp.text)
+        raise e
     return [
         stock(
             code=r["SECURITY_CODE_A"].strip(),
@@ -78,12 +96,12 @@ def _get_stock_list_by_page(page: int, stock_type) -> List[stock]:
 
 def get_stock_list(stock_type) -> List[stock]:
     """
-    循环调用 _get_stock_list_by_page 获取全部股票列表
+    循环调用 get_stock_list_by_page 获取全部股票列表
     """
     page = 1
     stock_list = []
     while True:
-        p = _get_stock_list_by_page(page, stock_type)
+        p = get_stock_list_by_page(page, stock_type)
         if not p:
             break
         page += 1
@@ -91,8 +109,8 @@ def get_stock_list(stock_type) -> List[stock]:
     return stock_list
 
 
-def store_stock_list(mongo_col, stock_list, market):
-    ops = [
+def _store_stock_list(mongo_col, stock_list, market):
+    op_list = [
         pymongo.UpdateOne(
             {"_id": stock.code},
             {
@@ -108,19 +126,17 @@ def store_stock_list(mongo_col, stock_list, market):
         )
         for stock in stock_list
     ]
-    mongo_col.bulk_write(ops)
+    mongo_col.bulk_write(op_list)
 
 
 if __name__ == "__main__":
-    from download.shse import db
-
     STOCK_TYPE_MAIN = 1
     STOCK_TYPE_KCB = 8
 
-    # 主板
+    # 上证主板
     main = get_stock_list(STOCK_TYPE_MAIN)
-    store_stock_list(db.Stock, main, "sh")
+    _store_stock_list(db.Stock, main, "sh")
 
     # 科创板
     kcb = get_stock_list(STOCK_TYPE_KCB)
-    store_stock_list(db.Stock, kcb, "kcb")
+    _store_stock_list(db.Stock, kcb, "kcb")
