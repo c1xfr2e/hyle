722e30ebe28c5cc4ce5ea61f4242cc795d4f2cee
bluecover <gurity@163.com>
2021-04-18 10:52:21 +0800

    Refactor integrate/company_position.py

--- integrate/company_position.py
+++ integrate/company_position.py
@@ -2,7 +2,11 @@
 # coding: utf-8
 
 """
-    基金公司整体调仓情况
+    统计基金公司整体持仓
+
+    Preconditions:
+        - mongodb documents: fund_company, fund
+        - fund 中已有 position_history 数据
 """
 
 import pymongo
@@ -22,48 +26,53 @@ def _add_position(cur, p, fund):
     }
 
 
-def _aggregate_funds_of_company(fund_list):
+def aggregate_fund_position_history(fund_list):
+    """
+    把多家基金的持仓历史累加，形成统一的 position
+
+    Args:
+        fund_list: 基金列表
+
+    Returns:
+        position_history dict, key 为 date, value 为 position dict:
+            {
+                "2020-12-31": {
+                    "600001": {"name": ..., "code": ...}
+                }
+            }
+    """
     position_history_dict = {}
     for fund in fund_list:
-        for pos_date in fund["position_history"]:
-            stock_position_dict = position_history_dict.setdefault(pos_date["date"], {})
-            for pos in pos_date["position"]:
-                stock_position_dict[pos["code"]] = _add_position(
-                    stock_position_dict.setdefault(pos["code"], {}),
-                    pos,
+        for ph in fund["position_history"]:
+            position_dict = position_history_dict.setdefault(ph["date"], {})
+            for p in ph["position"]:
+                position_dict[p["code"]] = _add_position(
+                    position_dict.setdefault(p["code"], {}),
+                    p,
                     fund,
                 )
     return position_history_dict
 
 
-def _position_history_dict_to_list(position_history_dict):
-    list_ = list(position_history_dict.values())
-    list_.sort(
+def _position_dict_to_list(position_dict):
+    lst = list(position_dict.values())
+    lst.sort(
         key=lambda x: x["float_percent"] * 100000 + x["net_percent"],
         reverse=True,
     )
-    return list_
+    return lst
 
 
-def _round_floats(position_list):
-    for p in position_list:
+def _round_floats(position):
+    for p in position:
         p["quantity"] = round(p["quantity"], 2)
         p["value"] = round(p["value"], 2)
         p["float_percent"] = round(p["float_percent"], 3)
         p["net_percent"] = round(p["net_percent"], 2)
-    return position_list
+    return position
 
 
-def _write_op(company, position_history_dict):
-    stock_position_history = [
-        {
-            "date": date,
-            "position": _round_floats(
-                _position_history_dict_to_list(position_history_dict[date]),
-            ),
-        }
-        for date in sorted(position_history_dict.keys(), reverse=True)
-    ]
+def _write_op(company, position_history_list):
     return pymongo.UpdateOne(
         {
             "_id": company["_id"],
@@ -72,7 +81,7 @@ def _write_op(company, position_history_dict):
             "$set": {
                 "co_name": company["name"],
                 "co_size": company["size"],
-                "position_history": stock_position_history,
+                "position_history": position_history_list,
             }
         },
         upsert=True,
@@ -89,7 +98,15 @@ def _fund_filter():
 if __name__ == "__main__":
     write_op_list = []
     for co in list(db.FundCompany.find()):
-        funds_of_company = list(db.Fund.find(_fund_filter()))
-        position_history_dict = _aggregate_funds_of_company(funds_of_company)
-        write_op_list.append(_write_op(co, position_history_dict))
+        fund_list = list(db.Fund.find(_fund_filter()))
+        position_history_dict = aggregate_fund_position_history(fund_list)
+        # 转成 list 保存
+        position_history_list = [
+            {
+                "date": date,
+                "position": _round_floats(_position_dict_to_list(position_history_dict[date])),
+            }
+            for date in sorted(position_history_dict.keys(), reverse=True)
+        ]
+        write_op_list.append(_write_op(co, position_history_list))
     db.FundCompanyPosition.bulk_write(write_op_list)
