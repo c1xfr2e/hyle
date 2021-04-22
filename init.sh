bc65e2370923a0bdf5265ede050d509ba6899c39
bluecover <gurity@163.com>
2021-04-22 22:28:40 +0800

    fix typo

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 . ./venv/bin/activate
 
 echo "下载沪深股票列表"
-python3 -m download.eastmoney.sotck.stock_list
+python3 -m download.eastmoney.stock.stock_list
 
 echo "下载基金持仓"
 python3 -m download.eastmoney.fund.fund_position
