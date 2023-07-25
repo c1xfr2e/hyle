c1c2bf57e3c58b8b20f1fccc72949d893cfa54a5
c1xfr2e <gurity@163.com>
2023-07-25 09:18:27 +0800

    Comment

--- download/eastmoney/stock/stock_list.py
+++ download/eastmoney/stock/stock_list.py
@@ -50,8 +50,6 @@ def get_stock_list_by_page(session, page_no, page_size) -> List[Dict]:
     Returns:
         股票列表, 带基本信息, 见 samples/stock_list.json
 
-http://62.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124021881304596364082_1690103811240&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1690103811241
-
         fetch("http://62.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124021881304596364082_1690103811240&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1690103811241", {
         "headers": {
             "accept": "*/*",
@@ -64,6 +62,10 @@ http://62.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124021881304596364082_1
         "mode": "cors",
         "credentials": "include"
         });
+
+    Updates:
+        2023-07-23:
+            停牌的股票没有数据, 返回的都是"-". 只能再用雪球的 stock_profile 再挨个取.
     """
 
     url = "http://62.push2.eastmoney.com/api/qt/clist/get"
